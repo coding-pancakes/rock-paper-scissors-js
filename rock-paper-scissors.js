5b98310858b768e3e5c231f742c8700bf8b238d8
giannag27 <giacosey28@gmail.com>
2025-10-24 16:15:38 -0400

    deleted if statement

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,15 +1,3 @@
 let player = 0;
 let computer = Math.floor(Math.random() * 1);
 
-if (player === 0 || computer === 0) {
-    console.log("Rock");
-}
-else if (player === 1 || computer === 1) {
-    console.log("Paper");
-}
-else if (player === 2 || computer === 2) {
-    console.log("Scissors");
-}
-else {
-    console.log("TIE!")
-}
