16c9956fe04e67b897cbd69dec79cfc56552fca9
giannag27 <giacosey28@gmail.com>
2025-10-24 16:19:37 -0400

    if statement and comparing values

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,17 @@
 let player = 0;
 let computer = Math.floor(Math.random() * 1);
 
+const rock = 0;
+const paper = 1;
+const scissors = 2;
+
+console.log("Player picked: " + player);
+console.log("Computer picked: " + computer);
+
+
+
+
+
+if (player === computer) {
+    console.log("It's a tie!");
+}
